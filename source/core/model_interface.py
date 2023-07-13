57e22404bba167807045a9d9efc3e814263aab22
ZRay07 <rayz1@wit.edu>
2023-07-13 00:27:51 -0400

    feat: Add test file for model_interface module
    -New test file, model_interface_test.py, to test the functionality of the model_interface.py module.
    -The test file includes test cases for audio recording, model loading, and transcription functionality.
    
    refactor: Refactor model_interface.py for improved structure and error handling
    
    - Recorder class: The `record` method now uses a try-except block to handle exceptions during audio recording. It returns `True` if the recording is successful and `False` otherwise.
    - ASR_model class: The `__init__` method now uses a try-except block to handle exceptions during the loading of the ASR processor and model. Error messages are printed for any loading failures.
    - ASR_model class: The `use_model` method now uses a try-except block to handle exceptions during audio processing and transcription. Error messages are printed for any transcription failures. It returns `True` if the transcription is successful and `False` otherwise.

--- source/core/model_interface.py
+++ source/core/model_interface.py
@@ -1,57 +1,76 @@
-from transformers import WhisperProcessor, WhisperForConditionalGeneration, AutoModelForSpeechSeq2Seq,  AutoProcessor
+from transformers import AutoModelForSpeechSeq2Seq,  AutoProcessor
 import numpy as np
 import soundfile as sf
-from datasets import load_dataset
-
 import sounddevice as sd
-from scipy.io import wavfile
 import wavio as wv
 
 # Set the path for recording
 RECORD_PATH = "data/output.wav"
 
 class Recorder:
-
     def __init__(self):
         self.recordFrequency = 16000
 
     def record(self, recordDuration):
-        print("\nListening...")
- 
-        # Start recorder with the given values
-        self.recording = sd.rec(int(recordDuration * self.recordFrequency),
-                                samplerate = self.recordFrequency, channels = 1, dtype = np.float64)
- 
-        # Record audio for the given number of seconds
-        sd.wait()
- 
-        # Convert the NumPy array to audio file
-        wv.write(RECORD_PATH, self.recording, self.recordFrequency, sampwidth = 1)
+        try:
+            print("\nListening...")
+    
+            # Start recorder with the given values
+            self.recording = sd.rec(int(recordDuration * self.recordFrequency),
+                                    samplerate = self.recordFrequency, channels = 1, dtype = np.float64)
+    
+            # Record audio for the given number of seconds
+            sd.wait()
+    
+            # Convert the NumPy array to audio file
+            wv.write(RECORD_PATH, self.recording, self.recordFrequency, sampwidth = 1)
+
+            return True
+        
+        except Exception as e:
+            print(f"Error occured during transcription: {e}")
+            return False
+
 
 class ASR_model:
     def __init__(self):
-        self.processor = AutoProcessor.from_pretrained("openai/whisper-tiny.en")
-        self.model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-tiny.en")
+        try:
+             # Load the ASR processor and model from pre-trained checkpoints
+            self.processor = AutoProcessor.from_pretrained("openai/whisper-tiny.en")
+            self.model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-tiny.en")
+            print("Model & processor loaded successfully")
+        
+        except Exception as e:
+            print(f"Error occured during processor/model loading: {e}")
         
     def use_model(self, pathToAudio):
+        try:
+            # Read audio data from the specified path
+            self.data, self.srate = sf.read(pathToAudio)
 
-        self.data, self.srate = sf.read(pathToAudio)
+            print("Processing...")
 
-        print("Processing...")
-#sample = ds["audio"]
-#audio = np.load(audio_path, allow_pickle = True)
-        self.input_features = self.processor(self.data, sampling_rate = 16000, return_tensors ="pt").input_features
-        self.predicted_ids = self.model.generate(self.input_features, max_length = 448)
-        self.transcription = self.processor.batch_decode(self.predicted_ids, skip_special_tokens=True)
+            # Preprocess the audio data using the processor
+            self.input_features = self.processor(self.data, sampling_rate = 16000, return_tensors ="pt").input_features
 
-        self.formattedTranscription = self.transcription[0]
-        self.formattedTranscription = self.formattedTranscription[1:]
-        return self.formattedTranscription
+            # Generate the transcription using the ASR model
+            self.predicted_ids = self.model.generate(self.input_features, max_length = 448)
 
+            # Decode the predicted IDs into text using the processor
+            self.transcription = self.processor.batch_decode(self.predicted_ids, skip_special_tokens=True)
 
-microphone = Recorder()
-whisper = ASR_model()
+            # Format the transcription result
+            self.formattedTranscription = self.transcription[0]
+            self.formattedTranscription = self.formattedTranscription[1:]
+            return self.formattedTranscription
+        
+        except Exception as e:
+            print(f"Error occured during transcription: {e}")
+            return False
+
 
 if __name__ == "__main__":
-    print("This should only run if called from cmd line")
+    # Create instances of Recorder and ASR_model
+    microphone = Recorder()
+    whisper = ASR_model()
 
